24dde71e206fcaab05c0878581b790a9a3ee2ed6
Carolina Arata <carolina.arata@cern.ch>
2025-01-31 22:57:48 +0100

    Added Combine0_30 for Systematics N Centr bin Mix

--- RunCombine.sh
+++ RunCombine.sh
@@ -4,12 +4,12 @@
 ###########################################################################################
 #void Combine0_30(float ptMin, float ptMax, int iCen = 0, bool bMirror = Mirror ON/OFF, TString shshBkg = "0.40-1.00", TString dirFiles = "Directory/wih/the/files/from aanalysis", double systPur = 1, 0.9, 1.1, bool bZYAM = ZYAM ON/OFF, bool bPlot = true, TString dirPlot = "where/to/save/the/plot")
 
-#root -b -l <<EOF
-#.L ~/work/histogram/IsoPhotonHadronCorrelations/Combine0_30.C
-#Combine0_30( 18, 40, 0, true, "0.40-1.00", "~/work/histogram/FromScratch/checkCode", 1, false, true, "~/work/histogram/FromScratch/FigcheckCode")
-#.q
-#EOF
-##
+root -b -l <<EOF
+.L ~/work/histogram/IsoPhotonHadronCorrelations/Combine0_30.C
+Combine0_30( 18, 40, 0, true, "0.40-1.00", "~/work/histogram/FromScratch/checkCode", 1, false, true, "~/work/histogram/FromScratch/FigcheckCode")
+.q
+EOF
+#
 #----------------------------------------------------------------------------------------------
 ###############################################################################################
 ################# Combine 0-10% and 10-30% for systematics on purity ##########################
@@ -32,25 +32,11 @@ EOF
 ########Combine 0-10% and 10-30% for systematics on N centrality bins used for mixed ###########
 ################################################################################################
 #----------------------------------------------------------------------------------------------
-#root -b -l <<EOF
-#.L ~/work/histogram/FromScratch/IsoGammaHadron.C
-#IsoGammaHadron(18, 40, "OLDDatasetConfigPtAssoc200_ptIso2GeV_diffShSh/AllTogether", true, "0.40-2.00", "SystematicsNMix45_ZtMergedMore", 1, false, TMath::Pi() * 3 / 5, TMath::Pi())
-#.q
-#EOF
-
-#root -b -l <<EOF
-#.L ~/work/histogram/FromScratch/IsoGammaHadron.C
-#IsoGammaHadron(18, 40, "OLDDatasetConfigPtAssoc200_ptIso2GeV_diffShSh/AllTogether", true, "0.40-2.00", "SystematicsNMix9_ZtMergedMore", 1, false, TMath::Pi() * 3 / 5, TMath::Pi())
-#.q
-#EOF
-
-#
-#root -b -l <<EOF
-#.L ~/work/histogram/FromScratch/IsoGammaHadron.C
-#IsoGammaHadron(16, 40, "DataSh100_AssocPt500", true, "0.40-1.00", "SystematicsNMix45_ZtMerged", 1, false, TMath::Pi() * 3 / 5, TMath::Pi())
-#.q
-#EOF
-
+root -b -l <<EOF
+.L ~/work/histogram/IsoPhotonHadronCorrelations/Combine0_30.C
+Combine0_30( 18, 40, 0, true, "0.40-1.00", "~/work/histogram/FromScratch/checkCodeSystNCentrMix", 1, false, true, "~/work/histogram/FromScratch/FigcheckCodeSystNCentrMix")
+.q
+EOF
 #-----------------------------------------------------------------------------------------------
 ################################################################################################
 ########################### Run the analysis for systematics on ShSh ###########################
